1b04af09a859b66f4b97d9ed6031600163e2e6d9
tdoval <thiagodovalaraujo@gmail.com>
2022-09-16 13:13:40 -0300

    template engine learning

--- base/views.py
+++ base/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('Home page')
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+    {'id': 4, 'name': 'Software Engineers'},
 
-def room(request):
-    return HttpResponse('ROOM')
+]
+
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
